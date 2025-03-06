05894a08ecfdbca8fd3bee30a3e6990d3b302b98
Serra <igor.potard@gmail.com>
2025-03-06 20:55:46 +0100

    serra

--- content.js
+++ content.js
@@ -8,7 +8,6 @@ function find_a(el) {
   return find_a(el.parentElement);
 }
 
-// Function to add the current hash to a database
 function addHashToDB(hash) {
   browserAPI.storage.local.get({ hashes: [] }).then((result) => {
     let hashes = result.hashes;
@@ -19,13 +18,11 @@ function addHashToDB(hash) {
   });
 }
 
-// Function to check if the database contains the hash
 async function isHashInDB(hash) {
   let result = await browserAPI.storage.local.get({ hashes: [] });
   return result.hashes.includes(hash);
 }
 
-// Function to check if the database contains the hash
 async function getDB() {
   let result = await browserAPI.storage.local.get({ hashes: [] });
   return result.hashes;
@@ -46,7 +43,7 @@ function add_pack_display() {
     packDisplayer.style.width = "100%";
     packDisplayer.style.height = "100%";
     packDisplayer.style.display = "none";
-    packDisplayer.style.backgroundColor = "rgba(0, 0, 0, 0.8)"; // Dark background with opacity
+    packDisplayer.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
     document.body.appendChild(packDisplayer);
   }
 }
@@ -54,7 +51,7 @@ function add_pack_display() {
 function addPackToDisplay(pack) {
   let packDisplayer = document.getElementById("pack-displayer");
   if (packDisplayer) {
-    packDisplayer.innerHTML = ""; // Clear previous pack if exists
+    packDisplayer.innerHTML = "";
     packDisplayer.appendChild(pack);
     packDisplayer.style.display = "flex";
     packDisplayer.addEventListener("click", closePackDisplay);
@@ -65,8 +62,7 @@ function closePackDisplay() {
   let packDisplayer = document.getElementById("pack-displayer");
   if (packDisplayer) {
     packDisplayer.style.display = "none";
-    packDisplayer.innerHTML = ""; // Clear the display
-    //document.location.reload(); // Reload the page to reset the display
+    packDisplayer.innerHTML = "";
   }
 }
 
@@ -75,12 +71,12 @@ async function add_open_pack_button(traceSymbol) {
     traceSymbol.getAttribute("errorstatus") != "" ||
     traceSymbol.getAttribute("status") != "SUCCEEDED"
   )
-    return; // Skip error status
+    return;
 
   a_tag = find_a(traceSymbol);
   var link = a_tag.href;
 
-  if ((await isHashInDB(link)) == true) return; // Skip if already processed
+  if ((await isHashInDB(link)) == true) return;
 
   let percentage = traceSymbol.getAttribute("successpercent");
   let button = document.createElement("button");
@@ -101,15 +97,13 @@ async function add_open_pack_button(traceSymbol) {
 		gototrace.style.cursor = "pointer";
 		gototrace.style.margin = "5px";
 
-  traceSymbol.setAttribute("data-processed", "true"); // Prevent duplicate processing
+  traceSymbol.setAttribute("data-processed", "true");
   traceSymbol.style.display = "none";
 
 		traceSymbol.parentNode.insertBefore(button, traceSymbol);
 		traceSymbol.parentNode.insertBefore(gototrace, traceSymbol);
 
   a_tag.href = "#";
-  //console.log(a_tag);
-  //console.log(traceSymbol);
 
   traceSymbol.setAttribute("link", link);
 
@@ -121,10 +115,6 @@ async function add_open_pack_button(traceSymbol) {
 	gototrace.addEventListener("click", function () {
 			window.location.href = link;
 		});
-
-  //a_tag.addEventListener("click", function () {
-			//window.location.href = link;
-  //});
 
 }
 
@@ -159,7 +149,7 @@ async function replaceTraceSymbols() {
     }
 
     if (!is_inside) {
-				all_in_list[i].classList.innerHTML = "?";//remove("list__item__secondary");
+				all_in_list[i].classList.innerHTML = "?";
       var checkMark =
         all_in_list[i].getElementsByClassName("list__item__right")[0];
       if (checkMark) checkMark.innerHTML = "?";
@@ -167,15 +157,14 @@ async function replaceTraceSymbols() {
   }
 }
 
-// Use MutationObserver to track dynamically loaded content
 const observer = new MutationObserver(replaceTraceSymbols);
 observer.observe(document.body, { childList: true, subtree: true });
 
-replaceTraceSymbols(); // Initial replacement in case elements are already present
+replaceTraceSymbols();
 
 function make_confetis() {
   const confettiWrapper = document.querySelector(".confetti-wrapper");
-  if (confettiWrapper.children.length > 0) return; // Prevent infinite confetti
+  if (confettiWrapper.children.length > 0) return;
 
   // Generate confetti
   for (let i = 0; i < 30; i++) {
@@ -188,7 +177,7 @@ function make_confetis() {
   }
 
   setTimeout(() => {
-    confettiWrapper.innerHTML = ""; // Remove confetti after animation
+    confettiWrapper.innerHTML = "";
   }, 4000);
 
   function getRandomColor() {
@@ -230,7 +219,6 @@ async function openPackAnimation(button, inputPercentage) {
   pack.appendChild(pack_text);
 
   addPackToDisplay(pack);
-  //button.parentNode.insertBefore(pack, button);
   pack.classList.add("spinning");
 
   href = button.parentElement
@@ -239,74 +227,39 @@ async function openPackAnimation(button, inputPercentage) {
 
   setTimeout(() => {
     let currentPercentage = 100;
-    let decreaseTime = 1000; // 1 second total decrease time
-    let steps = 10; // Number of steps
+    let decreaseTime = 1000;
+    let steps = 10;
     let stepTime = decreaseTime / steps;
 
     let percentages = [
       Math.floor(Math.random() * 100),
       inputPercentage,
     ];
-
     pack_text.style.color = get_color(get_image(currentPercentage));
-
-    let i = 0;
-
-    let decrement = (100 - percentages[i]) / steps;
-
+		let random_number = 20;
     let interval = setInterval(() => {
-      if (currentPercentage > percentages[i]) {
-        // Generate a random variation
-        currentPercentage -= decrement;
-        currentPercentage = Math.max(currentPercentage, percentages[i]); // Ensure it doesn't go below target
+				pack.classList.remove("spinning");
 
+        currentPercentage =  Math.floor(Math.random() * 100);
         pack_text.textContent = Math.floor(currentPercentage);
         pack_text.style.color = get_color(get_image(currentPercentage));
         pack.style.backgroundImage = `url(${get_url(currentPercentage)})`;
-      } else {
-        pack_text.textContent = percentages[i];
-
-        pack.style.backgroundImage = `url(${get_url(currentPercentage)})`;
-
-        if (i == percentages.length - 1) {
+        if (random_number == 0) {
+						pack_text.textContent = Math.floor(inputPercentage);
+						pack_text.style.color = get_color(get_image(inputPercentage));
+						pack.style.backgroundImage = `url(${get_url(inputPercentage)})`;
           clearInterval(interval);
-          if (percentages[i] == 100) {
+          if (inputPercentage == 100) {
             make_confetis();
-          } else if (percentages[i] == 0) {
-            pack.style.backgroundImage = `url(${browserAPI.runtime.getURL(
-              "img/rip_bozo.jpg"
-            )})`;
-
-            pack.style.width = "744px";
-            pack.style.height = "609px";
-            pack_text.style.display = "none";
           }
-
           pack.style.cursor = "pointer";
-
-          let span = document.createElement("span");
-          span.textContent = "Click on the card to see your trace";
-          span.style.color = "white";
-          span.style.position = "absolute";
-          span.style.bottom = "10px";
-          span.style.left = "50%";
-          span.style.transform = "translateX(-50%)";
-          span.style.fontSize = "20px";
-          span.style.fontWeight = "bold";
-          document.getElementById("pack-displayer").appendChild(span);
-
           addHashToDB(href);
-
           pack.addEventListener("click", function () {
+							console.log(href);
             document.location.href = href;
           });
-        }
-
-        i++;
-
-        decrement = (100 - percentages[i]) / steps;
-        currentPercentage = 100;
       }
-    }, stepTime);
+				random_number -= 1;
+    }, 200);
   }, 1000);
 }
